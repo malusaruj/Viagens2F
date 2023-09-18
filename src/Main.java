ee6819155afceabdd9d128d871adcb26b9debafa
malusarauj <maria.luizasa06@gmail.com>
2023-09-18 09:59:43 -0300

    Criado mais um construtor e entrada dinâmica

--- src/Main.java
+++ src/Main.java
@@ -28,8 +28,12 @@ public class Main {
         String cpf = JOptionPane.showInputDialog("Entre com cpf:");
 
         // Adicionar cpf no objeto pessoaTurista
-        pessoaTurista.setCpf(cpf);
-        System.out.println(pessoaTurista.getCpf());
+        if(Validacao.cpf(cpf)) {
+            pessoaTurista.setCpf(cpf);
+            System.out.println(pessoaTurista.getCpf());
+        } else {
+            System.out.println("CPF inválido");
+        }
 
     }
 }
